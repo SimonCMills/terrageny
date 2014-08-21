e661f690ac24d02e7f1a048dd72bb8e39738ce82
SimonCMills <simon.mills13@imperial.ac.uk>
2014-08-21 09:10:58 +0100

    added fragStats

---
+++ code/functions/getFragStats.R
@@ -0,0 +1,87 @@
+## Script to extract fragmentation metrics (will probably be written into 
+## a function at a later date)
+
+## Function: calculate size of fragments
+fragSizes <- function(clumpedRast, clump.ids="") {
+  if(clump.ids=="") {clump.ids <- 1:clumpedRast@data@max}
+  require(raster)
+  size.vect <- rep(NA, clumpedRast@data@max)
+  bs <- blockSize(clumpedRast)
+  for (i in 1:bs$n) {
+    val <- getValues(clumpedRast, row=bs$row[i], nrows=bs$nrows[i])
+    size <- table(val)
+    size.vect.temp <- rep(NA, clumpedRast@data@max)
+    size.vect.temp[as.numeric(rownames(size))] <- as.numeric(size)
+    size.vect <- rowSums(cbind(size.vect, size.vect.temp), na.rm=T)  
+  }
+  return(size.vect)
+}
+
+## Function: nearest neighbour
+nearestNeighbour <- function(clumpedRast, clump.ids="", size.vect="") {
+  require(raster)
+  if(clump.ids=="") {clump.ids <- 1:clumpedRast@data@max}
+  if(size.vect=="") {size.vect<- fragSizes(clumpedRast)}
+  
+  # ignore clumps over ten% of area
+  clump.ids[size.vect > ncell(clumpedRast)/10] <- NA
+     
+  for (i in clump.ids) {
+    print(i)
+    cellList <- which(clumpedRast[] == i)
+    # do this twice (by definition no NAs in first adjacent)
+    adj <- adjacent(clumpedRast, cells=cellList, directions=8, pairs=F )
+    if(any(adj %in% cellList)) {adj <- adj[-which(adj %in% cellList)]}
+    adj <- adjacent(clumpedRast, cells=adj, directions=8, pairs=F)
+    if(any(adj %in% cellList)) {adj <- adj[-which(adj %in% cellList)]}
+
+    dist.counter <- 1
+    while(!any(!is.na(clumpedRast[adj]))) {
+      print("looping")
+      dist.counter <- dist.counter + 1
+      adj <- adjacent(clumpedRast, cells=adj, directions=8, pairs=F)
+      if(any(adj %in% cellList)) {adj <- adj[-which(adj %in% cellList)]}
+    }
+    distance.vect[i] <- dist.counter
+  }
+  return(distance.vect)
+}
+
+# ## Function: calculate shape index of fragments 
+# fragSI <- function(clumpedRast, clump.ids="", size.vect="") {
+#   if(clump.ids=="") {clump.ids <- 1:clumpedRast@data@max}
+#   if(size.vect=="") {size.vect<- fragSizes(clumpedRast)}
+#   require(raster)
+#   shape.vect <- rep(NA, clumpedRast@data@max)
+#   bs <- blockSize(clumpedRast)
+#   for (i in 1:bs$n) {
+#     val <- getValues(clumpedRast, row=bs$row[i], nrows=bs$nrows[i])
+#     shape <- table(val)
+#     shape.vect.temp <- rep(NA, clumpedRast@data@max)
+#     shape.vect.temp[as.numeric(rownames(size))] <- as.numeric(size)
+#     size.vect <- rowSums(cbind(size.vect, size.vect.temp), na.rm=T)  
+#   }
+#   return(size.vect)
+# }
+
+# using SDMTools
+fragSI <- function(clumpedRast) {
+  require(raster)
+  require(SDMTools)
+  patchStats <- PatchStat(clumpedRast)
+  pSI <- patchStats$shape.index
+  return(pSI)
+}
+
+### Landscape stats
+proportionForest <- function(clumpedRast) {
+  require(raster)
+  return(length(which(clumpedRast[] != 1))/ncell(clumpedRast))
+}
+
+totalEdge <- function(clumpedRast) {
+  require(raster)
+  patchStats <- PatchStat(clumpedRast)
+  perimeter <- patchStats$perimeter
+  return(sum(perimeter))
+}
--- code/getTerraStats.R
+++ code/getTerraStats.R
@@ -1,6 +1,15 @@
-
-load("scenes/scene_665/clumped/terrageny.RData")
-View (terra.2)
+#library(raster)
+rm(list=ls())
+load("terrageny.RData")
+#load("../terragenyLocal/terragenyConstruct/manaus/Results.RData")
+#
+#source("code/functions/createTerrUT.R")
+#source("code/functions/constructTerrageny_ver2.R")
+#createTerrUT(unitTestDir="ut_terrstats/", yearRange=2000:2012)
+#constructTerrageny(inPath="ut_terrstats/dynamicSingle/", outPath="ut_terrstats/", inFileStart="year_", 
+#                   fileType=".tif", overwrite=T)
+#load("ut_terrstats/terrageny.RData")
+#View (terra.2)
 
 # interested in fragments that exist in 2013
 terra <- terra.2[which(terra.2$end.type == "exists"),]
@@ -10,7 +19,6 @@ terra <- terra[-which(terra$ancestor.end.ID == ""),]
 nExists <- length(terra$start.ID)
 # constructing pairwise nodes matrix
 pairwiseMatrix_nodes <- matrix(NA, nrow=nExists, ncol=nExists)
-
 # creating matrix containing fragment histories (recording each split)
 nodesMatrix <- matrix(NA, nrow=length(terra$start.ID), ncol=13, 
                       dimnames=list(1:length(terra$start.ID), 2012:2000))
@@ -40,51 +48,14 @@ for (i in 1:length(terra$start.ID)) {
   nodesMatrix[i, frag_i_ancestor.t] <- frag_i_ancestor
 }
 
-
 ################################################################################
-## JUNK??
-for (row_i in 1:nrow(nodesMatrix)) {  
-  # getting fragment history for i
-  frag_i <- nodesMatrix[row_i,]
-  # creating matrix which can edit within this iterate
-  nodesMatrix_edit <- nodesMatrix
-  # setting to NA so comparisons aren't made with self
-  nodesMatrix_edit[row_i] <- NA
-  # going over this 
-  node_ID <- which(!is.na(frag_i))
-  # going through nodes for this fragment, 
-  for (node_i in node_ID[-1]) {
-    # getting row-ID of pairs with this fragment node
-    node_ID <- as.numeric(which(nodesMatrix_edit[,node_i] == frag_i[node_i]))
-    node
-    pairedNodes <- nodesMatrix_edit[node_ID,1:node_i]
-    pairedNodes
-    # creating vector that assumes every year split
-    nodesDefault <- rep(ncol(pairedNodes), nrow(pairedNodes))
-    nodesDefault
-    # creating 0-valued vector of same length
-    NAcount <- rep(0, nrow(pairedNodes))
-    # looping through years, counting years in which a split wasn't made (NA)
-    for (i in 1:ncol(pairedNodes)-1) {
-      colNAs <- which(is.na(pairedNodes[,i]))
-      NAcount[colNAs] <- NAcount[colNAs] + 1
-    }
-    # updating pairwise matrix
-    pairwiseMatrix_nodes[as.numeric(rownames(pairedNodes)), row_i] <- nodesDefault - NAcount
-    nodesMatrix_edit[rownames(pairedNodes),] <- NA
-  }  
-}
-################################################################################    
-
 pairwiseNodal <- matrix(NA, nrow=nExists, ncol=nExists)
-outNodal <- matrix(NA, nrow=nExists, ncol=nExists)
-inNodal <- matrix(NA, nrow=nExists, ncol=nExists)
 
 # loop over years in nodesMatrix (2011:2000)
-for (year_i in as.character(2000:2011)) {
+for (year_i in as.character(2000:2012)) {
   # get list of nodal events in year_i (ignoring NA)
   nodes <- unique(nodesMatrix[,year_i])
-  nodes <- nodes[-which(is.na(nodes))]
+  if (any(is.na(nodes))) {nodes <- nodes[-which(is.na(nodes))]}
   print("year:")
   print(year_i)
   # loop through nodal events, getting number of nodes separating pairs
@@ -104,12 +75,150 @@ for (year_i in as.character(2000:2011)) {
         nodalFragments_splits[fragment_i] <- nSplits
       }
       print(nodalFragments_splits)
-      pairwise <- matrix(NA, nrow=length(nodalFragments_splits), ncol=length(nodalFragments_splits))
+      pairwise <- matrix(NA, nrow=length(nodalFragments_splits), ncol=length(nodalFragments_splits), 
+                         dimnames=list(nodalFragments, nodalFragments))
       for (pair_i in 1:length(nodalFragments_splits)) {
-        pairwise[,pair_i] <- nodalFragments_splits + nodalFragments_splits[pair_i]
+        # -1 to account for shared node at split
+        pairwise[,pair_i] <- nodalFragments_splits + nodalFragments_splits[pair_i] - 1
+      }
+      for (i in colnames(pairwise)) {
+        for (j in rownames(pairwise)) {
+          pairwiseNodal[as.numeric(j), as.numeric(i)] <- pairwise[j,i]
+        }
       }
     }
   }
+  diag(pairwiseNodal) <- NA
 }
-splits
-nodalFragments_splits
+hist(pairwiseNodal)
+image(pairwiseNodal)
+################################################################################
+# function 2: computing terragenetic distinctiveness. 
+timestep <- as.character(2000:2012)
+
+# variables
+time.start <- as.character(2000)
+time.finish <- as.character(2012)
+
+# need to compute nodesMatrix from terra.2
+# interested in fragments that exist in 2013
+terra.exists <- which(terra.2$end.type == "exists")
+terra.extinct <- which(terra.2$end.type == "destroyed")
+# ignoring fragments which have remained unchanged since time 0 (and are therefore
+# completely unconnected)
+terra.exists <- terra.exists[-which(terra.2$ancestor.end.ID[terra.exists] == "")]
+n.exists <- length(terra.exists)
+# creating matrix containing fragment histories (recording each split)
+nodes.matrix <- matrix(NA, nrow=length(terra.exists), ncol=13, 
+                       dimnames=list(1:length(terra.exists), 2012:2000))
+
+
+# assigning fragment start node (as vector)
+start.yr.list <- terra.2$start.t[terra.exists]
+start.id.list <- terra.2$start.ID[terra.exists]
+# assigning fragment ancestor node
+ancestor.list <- as.character(terra.2$ancestor.end.ID[terra.exists])
+# getting year-component of this 
+ancestor.t <- substr(ancestor.list, 1, 4)
+
+# looping through fragments to construct history
+# note: this matrix does not retain original row ids, these are kept in terra.exists
+for (i in 1:n.exists) {
+  nodes.matrix[i, as.character(start.yr.list)[i]] <- start.id.list[i]
+  # placing ID in matrix
+  nodes.matrix[i, ancestor.t[i]] <- terra.2$ancestor.end.ID[terra.exists[i]]
+  # assigning ancestor's ancestor node
+  frag.i <- terra.2[terra.exists[i],]
+  frag.i.ancestor <- frag_i$ancestor.end.ID
+  frag_i_ancestor.t <- substr(frag_i_ancestor, 1, 4)
+  nodesMatrix[i, frag_i_ancestor.t] <- frag_i_ancestor
+  #looping through
+  while(frag_i_ancestor.t != time.start) {
+    frag_i1 <- terra.2[which(terra.2$end.ID == frag_i_ancestor),]
+    frag_i_ancestor <- frag_i1$ancestor.end.ID
+    if(frag_i_ancestor == "") break()
+    frag_i_ancestor.t <- substr(frag_i1$ancestor.end.ID, 1, 4)
+    nodes.matrix[i, frag_i_ancestor.t] <- frag_i_ancestor
+  }
+  nodes.matrix[i, frag_i_ancestor.t] <- frag_i_ancestor
+}
+
+
+
+
+
+# loop over years in nodesMatrix (2011:2000)
+for (year_i in 1:length(timestep)) {
+  year_i <- "2001"
+  # get list of nodal events in year_i (ignoring NA)
+  nodes <- unique(nodesMatrix[,year_i])
+  nodes
+  if (any(is.na(nodes))) {nodes <- nodes[-which(is.na(nodes))]}
+  nodes
+  # counter
+  print("year:")
+  print(timestep[year_i])
+  
+  # loop through nodal events, getting number of nodes separating pairs
+  for (node_i in 1:length(nodes)) {
+    node_i <- 1
+    print(paste("node", node_i))
+    nodalFragments <- which(nodes.matrix[,year_i] == nodes[node_i])
+    
+    nodalDescendants <- which(terra.2$ancestor.end.ID == nodes[node_i])
+    nodalDescendants.1 <- which(terra.2$start.ID == nodes[node_i])
+    nodalDescendants.1
+    nodalDescendants
+    nodes[node_i]
+    terra.2[9,]
+    terra.2[35,]
+    terra.2[220,]
+    which(nodes.matrix[,"2004"] == "2004:1")
+    
+    nodalFragments
+    print(nodalFragments)
+    
+    # getting next timestep in which split happens
+    if (any(is.na(nodesMatrix[nodalFragments, timestep[year_i+1]]))) {
+      isNA <- TRUE
+      NAcount <- 1
+      while (any(is.na(nodesMatrix[nodalFragments, timestep[year_i+NAcount]]))) {
+        nodesMatrix[nodalFragments, timestep[year_i+NAcount]]
+        NAcount <- NAcount+1
+      }
+    }
+    timestep[year_i+NAcount]
+    unique(nodesMatrix[nodalFragments, timestep[year_i+NAcount]])
+    
+    nodesMatrix[nodalFragments, as.character(as.numeric(year_i)-)]
+    
+    nodesMatrix[nodalFragments,]
+    nodesMatrix[nodalFragments, as.character(as.numeric(year_i)-3)]
+    # if just one extant fragment associated with this node (i.e. itself), skip
+    if (length(nodalFragments) != 1) {
+      # creating blank vector of same length
+      nodalFragments_splits <- rep(NA, length(nodalFragments))
+      # looping through years, counting years in which a split was made (!=NA)
+      for (fragment_i in 1:length(nodalFragments)) {
+        yearVect <- 2012:as.numeric(year_i)
+        splits <- which(!is.na(nodesMatrix[nodalFragments[fragment_i], as.character(yearVect)]))
+        nSplits <- length(splits)
+        if (nSplits == 0) {stop("nSplits is 0")}
+        nodalFragments_splits[fragment_i] <- nSplits
+      }
+      print(nodalFragments_splits)
+      pairwise <- matrix(NA, nrow=length(nodalFragments_splits), ncol=length(nodalFragments_splits), 
+                         dimnames=list(nodalFragments, nodalFragments))
+      for (pair_i in 1:length(nodalFragments_splits)) {
+        # -1 to account for shared node at split
+        pairwise[,pair_i] <- nodalFragments_splits + nodalFragments_splits[pair_i] - 1
+      }
+      for (i in colnames(pairwise)) {
+        for (j in rownames(pairwise)) {
+          pairwiseNodal[as.numeric(j), as.numeric(i)] <- pairwise[j,i]
+        }
+      }
+    }
+  }
+  diag(pairwiseNodal) <- NA
+}
